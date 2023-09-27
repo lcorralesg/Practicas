b40496bd6c341dbd1340412630549b7592a252a0
LuisCorrales <luis.corrales@tecsup.edu.pe>
2023-09-27 18:33:05 -0500

    whitenoise

--- Practicas/settings.py
+++ Practicas/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Alumnos.apps.AlumnosConfig',
