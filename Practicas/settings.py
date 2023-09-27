0d1eff51954e5dc9d5fdec16592d7dfb7f43a9ea
LuisCorrales <luis.corrales@tecsup.edu.pe>
2023-09-27 18:35:42 -0500

    middleware

--- Practicas/settings.py
+++ Practicas/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Alumnos.apps.AlumnosConfig',
@@ -47,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
